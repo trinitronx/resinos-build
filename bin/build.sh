16f22c2456bda033a19cf888f251b9749795dfb7
James Cuzella <james.cuzella@lyraphase.com>
2017-03-09 13:00:36 -0700

    Fix docker GID to match host's /var/run/docker.sock file owner; Add & mount missing directories: /export, /data_disk

--- bin/build.sh
+++ bin/build.sh
@@ -2,7 +2,13 @@
 
 BUILD_HOST_OS='ubuntu:16.04'
 BUILD_TARGET='raspberrypi'
+DOCKER_GID=$(id -g docker)
 
-docker build -t trinitronx/build-resinos:${BUILD_TARGET}-${BUILD_HOST_OS//:/-} -f dockerfiles/${BUILD_HOST_OS//://}/Dockerfile .
+docker build --build-arg DOCKER_GID=${DOCKER_GID} -t trinitronx/build-resinos:${BUILD_TARGET}-${BUILD_HOST_OS//:/-} -f dockerfiles/${BUILD_HOST_OS//://}/Dockerfile .
 
-docker run -ti -v $(pwd)/build:/src/resin-raspberrypi/build/ -v /var/run/docker.sock:/var/run/docker.sock -v /var/run/docker.pid:/var/run/docker.pid -v /sys/fs/cgroup:/sys/fs/cgroup  --privileged --pid=host  --entrypoint=/bin/bash trinitronx/build-resinos:${BUILD_TARGET}-${BUILD_HOST_OS//:/-}
+[ -d /export ] || sudo mkdir -p /export
+[ -d /data_disk ] || sudo mkdir -p /data_disk
+sudo chown 1000:1000 /export
+sudo chown 1000:1000 /data_disk
+
+docker run -ti -v $(pwd)/build:/src/resin-raspberrypi/build/ --device=/dev/loop-control:/dev/loop-control -v /dev/loop0:/dev/loop0 -v /export:/export -v /data_disk:/data_disk  -v /var/run/docker.sock:/var/run/docker.sock -v /var/run/docker.pid:/var/run/docker.pid -v /sys/fs/cgroup:/sys/fs/cgroup  --privileged=true --pid=host  --cap-add=CAP_MKNOD  --entrypoint=/bin/bash trinitronx/build-resinos:${BUILD_TARGET}-${BUILD_HOST_OS//:/-}
